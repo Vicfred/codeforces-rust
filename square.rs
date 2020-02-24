827b3581ce110395a06ace08380d13d28e9c2e6c
Vicfred <vicfredboy@gmail.com>
2020-02-23 20:26:56 -0600

    square and waytoolong

---
+++ square.rs
@@ -0,0 +1,10 @@
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("error");
+    let words: Vec<i64> = input.split_whitespace().map(|s| s.parse().unwrap()).collect();
+
+    println!("{}", ((words[0] + words[2] - 1)/words[2]) * ((words[1] + words[2] - 1)/words[2]));
+}
+
---
+++ waytoolongwords.rs
@@ -0,0 +1,23 @@
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+    
+    io::stdin().read_line(&mut input)
+        .expect("");
+
+    let mut n = input.trim().parse::<i32>().unwrap();
+
+    while n > 0 {
+        let mut word = String::new();
+        io::stdin().read_line(&mut word)
+            .expect("");
+        if word.len() > 12 {
+            println!("{}{}{}", &word[0..1], word.len()-4, &word[word.len()-3..word.len()-1]);
+        } else {
+            println!("{}", &word[..word.len()-1])
+        }
+        n -= 1;
+    }
+
+}
