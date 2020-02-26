a7218cc1ad280c9de452ed6b7052e8fc9136ec92
Vicfred <vicfredboy@gmail.com>
2020-02-25 21:00:36 -0600

    football

---
+++ football.rs
@@ -0,0 +1,15 @@
+use std::io;
+
+fn main() {
+    let mut line = String::new();
+
+    io::stdin().read_line(&mut line)
+        .unwrap();
+
+    if line.contains("1111111") || line.contains("0000000") {
+        println!("YES");
+    } else {
+        println!("NO");
+    }
+}
+
