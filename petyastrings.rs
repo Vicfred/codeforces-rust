8a87077dec8c36cd30b5dd626833b6a5c7d70847
Vicfred <vicfredboy@gmail.com>
2020-02-25 20:53:42 -0600

    Petya and Strings

---
+++ petyastrings.rs
@@ -0,0 +1,26 @@
+use std::io;
+
+fn main() {
+    let mut first = String::new();
+
+    io::stdin().read_line(&mut first)
+        .unwrap();
+
+    let first = first.to_ascii_lowercase();
+
+    let mut second = String::new();
+
+    io::stdin().read_line(&mut second)
+        .unwrap();
+
+    let second = second.to_ascii_lowercase();
+
+    if first < second {
+        println!("-1");
+    } else if first > second  {
+        println!("1");
+    } else {
+        println!("0");
+    }
+}
+
