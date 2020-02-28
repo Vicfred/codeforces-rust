cf2c59fc2bf95d6a3e568a19a0cc2ebc97afd6a5
Vicfred <vicfredboy@gmail.com>
2020-02-27 21:20:45 -0600

    string slices

---
+++ wordcapitalization.rs
@@ -0,0 +1,12 @@
+use std::io;
+
+fn main() {
+    let mut word = String::new();
+
+    io::stdin()
+        .read_line(&mut word)
+        .unwrap();
+
+    print!("{}", &word[..1].to_uppercase());
+    print!("{}", &word[1..word.len()]);
+}
