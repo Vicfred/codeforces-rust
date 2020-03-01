71098a83917d8c189ef21bf2c4e8723204f12746
Vicfred <vicfredboy@gmail.com>
2020-02-29 21:52:08 -0600

    map, bucket sort

---
+++ boyorgirl.rs
@@ -0,0 +1,22 @@
+use std::io;
+use std::collections::HashMap;
+
+fn main() {
+    let mut letters = HashMap::new();
+    let mut username = String::new();
+
+    io::stdin()
+        .read_line(&mut username)
+        .unwrap();
+
+    for ch in username.chars() {
+        letters.insert(ch,1);
+    }
+
+    if letters.len() % 2 == 0 {
+        println!("CHAT WITH HER!");
+    } else {
+        println!("IGNORE HIM!");
+    }
+}
+
