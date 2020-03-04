bc15e0b44c6360213214f78a75878d95ab466879
Vicfred <vicfredboy@gmail.com>
2020-03-03 19:59:14 -0600

    precalculate, divisibility

---
+++ luckydivision.rs
@@ -0,0 +1,27 @@
+use std::io;
+
+fn main() {
+    let mut n = String::new();
+
+    io::stdin()
+        .read_line(&mut n)
+        .unwrap();
+
+    let n : i64 = n.trim().parse().unwrap();
+
+    let lucky = [4,7,44,47,74,444,447,474,477,744,747,777];
+
+    let mut flag = false;
+
+    for d in lucky.iter() {
+        if n % d == 0 {
+            flag = true;
+        }
+    }
+
+    if flag {
+        println!("YES");
+    } else {
+        println!("NO");
+    }
+}
