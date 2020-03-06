e7438f95e60a4c9a5a4a05ae98e47de3ccecc483
Vicfred <vicfredboy@gmail.com>
2020-03-05 21:06:37 -0600

    basic strings

---
+++ hq9.rs
@@ -0,0 +1,17 @@
+use std::io;
+
+fn main() {
+    let mut p = String::new();
+
+    io::stdin()
+        .read_line(&mut p)
+        .unwrap();
+
+    if p.contains("H") ||
+       p.contains("Q") ||
+       p.contains("9") {
+        println!("YES");
+    } else {
+        println!("NO");
+    }
+}
