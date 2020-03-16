3f7c2e423a8fec19e133e4a3d5f5fb4cedf5db0b
Unai Hernandez <unihernandez22@gmail.com>
2020-03-16 02:23:07 -0300

    Link y formateo

--- dominopiling.rs
+++ dominopiling.rs
@@ -1,12 +1,12 @@
+// https://codeforces.com/problemset/problem/50/A
 use std::io;
 
 fn main() {
     let mut input = String::new();
 
-    io::stdin().read_line(&mut input)
-        .unwrap();
+    io::stdin().read_line(&mut input).unwrap();
 
-    let words: Vec<i32>  = input
+    let words: Vec<i32> = input
         .split_whitespace()
         .map(|s| s.parse().unwrap())
         .collect();
@@ -14,5 +14,5 @@ fn main() {
     let n = words[0];
     let m = words[1];
 
-    println!("{}", n*m/2 as i32);
+    println!("{}", n * m / 2 as i32);
 }
